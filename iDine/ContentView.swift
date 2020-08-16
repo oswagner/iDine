3e78cf7299033455fdfc6201a22aa7372c1a44e0
Wagner Santos <wosantos@thoughtworks.com>
2020-08-16 20:06:48 -0300

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Wagner Oliveira dos Sants on 16/08/20.
+//  Copyright © 2020 Wagner Oliveira dos Sants. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
